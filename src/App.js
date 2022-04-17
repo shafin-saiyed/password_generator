0fb9c5f2398cc6b259e3a28cb8158dab19eeb69d
shafin <shafinsaiyed2624@gmail.com>
2022-04-17 15:32:53 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import './App.css';
+import './pages/PasswordGenerator.js'
+import PasswordGenerator from './pages/PasswordGenerator.js'
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordGenerator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/PasswordGenerator.js
@@ -0,0 +1,45 @@
+import React,{useState,useEffect} from 'react'
+import { CopyOutlined,ReloadOutlined } from '@ant-design/icons';
+import { message } from 'antd';
+import 'antd/dist/antd.css';
+import "../App.css";
+
+function PasswordGenerator() {
+    const [password, setPassword] = useState("");
+    useEffect(() =>{
+        generatePassword();
+    },[]);
+    const generatePassword = () => {
+        const randomPassword =
+          Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
+    
+        setPassword(randomPassword);
+      };
+      const copyPassWord = async() =>{
+        try {
+            await navigator.clipboard.writeText(password);
+            message.success("Password copied to your clipboard");
+        } catch {
+            message.error("Error copying password to clipboard");
+        }
+      }
+    return (
+        <div>
+            <div className="background">
+                <h1 className="heading">Generate a secure password</h1>
+                <div className="wrapper">
+                    <div className="password">
+                        <span>{password}</span>
+                        &nbsp;
+                        <span onClick={copyPassWord} style={{cursor:'pointer'}}><CopyOutlined /></span>
+                        &nbsp;
+                        <span onClick={generatePassword} style={{cursor:'pointer'}}><ReloadOutlined /></span>
+                    </div>
+                    <button className="generate-password" onClick={copyPassWord} style={{cursor:'pointer'}}>Copy password</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PasswordGenerator
