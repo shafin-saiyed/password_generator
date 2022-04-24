ce6fc4e6776e7cd50132d4bc944a2641be9eeee3
shafin <shafinsaiyed2624@gmail.com>
2022-04-24 14:17:34 +0530

    save changes

--- src/pages/PasswordGenerator.js
+++ src/pages/PasswordGenerator.js
@@ -1,19 +1,34 @@
 import React,{useState,useEffect} from 'react'
 import { CopyOutlined,ReloadOutlined } from '@ant-design/icons';
-import { message } from 'antd';
+import { message,Tooltip,Button,Slider,Card } from 'antd';
 import 'antd/dist/antd.css';
 import "../App.css";
+import AdSense from 'react-adsense';
 
 function PasswordGenerator() {
+    //const url = 'https://passwordinator.herokuapp.com/generate?num=true&caps=true&char=true&len=8';
     const [password, setPassword] = useState("");
+    const [passwordLen, setPasswordLen] = useState("10");
     useEffect(() =>{
         generatePassword();
-    },[]);
-    const generatePassword = () => {
-        const randomPassword =
-          Math.random().toString(36).slice(2);
+    },[passwordLen]);
+    const generatePassword = async () => {
+        console.log(passwordLen)
+        const url = 'https://passwordinator.herokuapp.com/generate?'+'len='+passwordLen;
+        await fetch(url).then(
+            response => response.json()
+            .then(
+                data => {
+                    console.log(data);
+                    setPassword(data.data);
+                }
+            )
+        )
     
-        setPassword(randomPassword);
+        //const randomPassword =
+          //Math.random().toString(36).slice(2);
+    
+        //setPassword(randomPassword);
       };
       const copyPassWord = async() =>{
         try {
@@ -23,21 +38,45 @@ function PasswordGenerator() {
             message.error("Error copying password to clipboard");
         }
       }
+      const lengthHandler = async (value) => {
+        console.log('onChange: ', value);
+        setPasswordLen(value);
+        console.log(passwordLen)
+        //generatePassword();
+      }
+
     return (
         <div>
             <div className="background">
-                <h1 className="heading">Generate a random password</h1>
-                <div className="wrapper">
-                    <div className="password">
+            <div className="site-card-border-less-wrapper">
+                <Card bordered={false} style={{ width: 1000 }}>
+                <div className="password">
                         <span>{password}</span>
                         &nbsp;
-                        <span onClick={copyPassWord} style={{cursor:'pointer'}}><CopyOutlined /></span>
+                        <span onClick={copyPassWord} style={{cursor:'pointer','color': 'darkred'}}><Tooltip title="Copy"><CopyOutlined /></Tooltip></span>
                         &nbsp;
-                        <span onClick={generatePassword} style={{cursor:'pointer'}}><ReloadOutlined /></span>
-                    </div>
-                    <button className="generate-password" onClick={copyPassWord} style={{cursor:'pointer'}}>Copy password</button>
+                        <span onClick={generatePassword} style={{cursor:'pointer','color': 'darkred'}}><Tooltip title="New"><ReloadOutlined /></Tooltip></span>
+                </div>
+                </Card>
+            </div>
+            <div className="site-card-border-less-wrapper">
+                <Card bordered={false} style={{ width: 1000 }}>
+                    <span style={{ width: 500 }}>
+                <Slider defaultValue={passwordLen} 
+                        tooltipVisible
+                        min={1}
+                        max={20}
+                        onChange={lengthHandler} />
+                    </span>
+                <Button shape="round" size="large" onClick={copyPassWord}><CopyOutlined /> Copy password</Button>
+                </Card>
                 </div>
             </div>
+            <AdSense.Google
+            client='ca-pub-8776734790123994'
+            slot='5119667375'
+            />
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </div>
     );
 }
